ccae56d5500277ac28f42f87d7fb254a90b976f7
liewchooichin <liewchooichin@gmail.com>
2025-02-07 14:23:22 +0800

    Done

--- App.js
+++ App.js
@@ -1,70 +1,145 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, ScrollView } from 'react-native';
-import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, Pressable, Button } from 'react-native';
+import { SafeAreaView, SafeAreaProvider  } from 'react-native-safe-area-context';
+import React, { useState, useMemo } from "react";
+import RadioGroup from 'react-native-radio-buttons-group';
 
-import monkeyPng from "./assets/monkey.png"
+// Images
+import catPng from "./assets/cat2.png"
+
 
 export default function App() {
 
   // text state
-  const[name, setName] = useState("Coconut");
-  const[age, setAge] = useState("40");
-  const[isCoffee, setIsCoffee] = useState(false);
+  const[name, setName] = useState("");
+  const[age, setAge] = useState("");
+  const[label, setButtonLabel] = useState("Submit");
+  const[likeCoffee, setLikeCoffee] = useState("null");
+  const[testCoffee, setTestCoffee] = useState("test");
+  const[isSubmitted, setIsSubmitted] = useState("false");
+
+  // set the radio button
+  function initializeRadioButtons(){
+    let buttons = [
+      {
+        id: "1",
+        label: "Yes",
+        value: "yes",
+        selected: "false",
+      }, 
+      {
+        id: "2",
+        label: "No",
+        value: "no",
+        selected: "false",
+      }
+    ];
+    return buttons;
+  }
+  // no dependency
+  const radioButtons = useMemo(()=> (initializeRadioButtons()), []);
 
   // event handler
   // text handler
-  function changeName(text){
-    setName(text);
+  function handleName(name){
+    setName(name);
   }
   // number handler
-  function changeAge(num){
+  function handleAge(num){
     setAge(num);
+  }
+  // choose radio buttons handler
+  function handleCoffee(selectedId, value){
+    if(selectedId === "1"){
+       setLikeCoffee("1");
+       setTestCoffee("Like coffee");
+    }
+    if(selectedId === "2"){
+      setLikeCoffee("2");
+      setTestCoffee("Don't like coffee");
+    }
+  }
+  // set the button label
+  function handleSubmitButton(){
+    setButtonLabel("You entered: ");
+    setIsSubmitted(true);
+  }
+  // reset the values
+  function handleResetButton(){
+    setName("");
+    setAge(null);
+    setLikeCoffee(null);
+    setButtonLabel("Submit");
+    setLikeCoffee(null);
+    setIsSubmitted(false);
   }
 
   return (
-    <View style={[styles.container]}>
+    <SafeAreaProvider style={[styles.container]}>
+      <SafeAreaView style={[styles.container]}>
 
-      <View style={[styles.rowContainer]}>
-        <Text style={[styles.text]}>Name</Text>
-        <TextInput
-          style={styles.input}
-          value={name}
-          placeholder={"Your name"}
-          onChangeText={(text) => changeName(text)}
-        ></TextInput>
-      </View>
-      <View style={[styles.rowContainer]}
+        <SafeAreaView style={[styles.rowContainer]}>
+        <Image
+          source={catPng}
+          style={[styles.image, styles.bannerImage]}
+        ></Image>
+        <Text style={[styles.text, styles.bannerText]}>Cat cafe</Text>
+        </SafeAreaView>
         
-      >
-        <Text style={[styles.text]}>Age</Text>
+        <SafeAreaView style={[styles.rowContainer]}> 
+        <Text style={[styles.text, styles.textForm]}>Nickname</Text>
+        <TextInput 
+          style={[styles.input]}
+          onChangeText={(value) => handleName(value)}
+          value={name}
+        />
+        </SafeAreaView>
+
+        <SafeAreaView style={[styles.rowContainer]}> 
+        <Text style={[styles.text, styles.textForm]}>Age</Text>
         <TextInput
-          style={styles.input}
+          style={[styles.input]}
+          onChangeText={(value) => handleAge(value)}
           value={age}
-          placeholder={"Your name"}
-          onChangeText={(num) => changeAge(num)}
-        ></TextInput>
-      </View>
-      <View style={[styles.rowContainer]}
-      
-      >
-        <Text style={[styles.text]}>Name</Text>
-        <Text style={[styles.text]}>Name</Text>
-      </View>
-      <View style={[styles.rowContainer]}
-      
-      >
-        <Text style={[styles.text]}>Name</Text>
-        <Text style={[styles.text]}>Name</Text>
-      </View>
+          keyboardType="numeric"
+        />
+        </SafeAreaView>
 
-    <View style={[styles.container]}
+        <SafeAreaView style={[styles.rowContainer]}>
+          <Text style={[styles.text, styles.textForm]}>Like coffee?</Text>
+          <RadioGroup
+            containerStyle={[styles.radioGroup]}
+            labelStyle={[styles.radioLabel]}
+            radioButtons={radioButtons}
+            onPress={(selectedId, value) => handleCoffee(selectedId, value)}
+            selectedId={likeCoffee}
+          ></RadioGroup>
+        </SafeAreaView>
 
-    >
-      <Text style={[styles.text]}>{name}</Text>
-      <Text style={[styles.text]}>{age}</Text>
-    </View>
+        <SafeAreaView style={[styles.buttonContainer]}> 
+          <Pressable 
+            style={[styles.button]} 
+            onPress={() => handleSubmitButton()}>
+            <Text style={[styles.buttonLabel]}>{label}</Text>
+          </Pressable>
+        </SafeAreaView>
+        
+        <SafeAreaView style={[styles.buttonContainer]}> 
+          <Pressable 
+            style={[styles.button]} 
+            onPress={() => handleResetButton()}>
+            <Text style={[styles.buttonLabel]}>Reset</Text>
+          </Pressable>
+        </SafeAreaView>
 
-    </View>
+        {(isSubmitted===true) && (<SafeAreaView style={[styles.responseContainer]}> 
+          <Text style={[styles.text]}>{name}</Text>
+          <Text style={[styles.text]}>{age}</Text>
+          <Text style={[styles.text]}>{testCoffee}</Text>
+        </SafeAreaView>)}
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 
 }
@@ -72,33 +147,91 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
     flexDirection: "column",
-    justifyContent: "center",
+    flex: 1,
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingTop: 3,
+    paddingBottom: 3,
+    //borderWidth: 1,
+    justifyContent: "space-around",
+    alignItems: "start",
   },
   rowContainer: {
-    flex: 1,
     flexDirection: "row",
+    //borderWidth: 1,
+    // paddingRight: 3,
+    // paddingLeft: 3,
+    // paddingTop: 10,
+    // paddingBottom: 10,
+    justifyContent: "start",
+    alignItems: "center",
   },
   text: {
-    flex: 1,
-    marginRight: "40",
     fontSize: 20,
     fontWeight: 500,
-    color: "green",
-    borderWidth: 1,
+    color: "blue",
+    //borderWidth: 1,
+  },
+  textForm: {
+    textAlign: "right",
+    paddingRight: 5,
+  },
+  bannerText: {
+    fontSize: 50,
+    textAlign: "center",
+    //borderWidth: 1,
   },
   image: {
-    height: 500,
-    width: 500,
+    height: 80,
+    width: 80,
+  },
+  bannerImage: {
+    //borderWidth: 1,
   },
   input: {
-    flex: 3,
+    flex: 1,
     height: 40,
-    margin: 12,
-    borderWidth: 3,
-    borderColor: "",
-    padding: 10,
+    margin: 3,
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 3,
   },
+  buttonContainer: {
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "blue",
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    borderColor: "white",
+    // justifyContent: "center",
+    // alignItems: "center",
+    width: "100%",
+  },
+  buttonLabel: {
+    color: "white",
+    fontSize: 40,
+    //borderWidth: 1,
+    width: "100%",
+    textAlign: "center",    
+  },
+  radioGroup: {
+    layout: "row",
+    //borderWidth: 1,
+    textAlign: "left",
+    width: "100%",
+    paddingRight: 10,
+  },
+  radioLabel: {
+    fontSize: 20,
+    width: "100%",
+  },
+  responseContainer: {
+    alignItems: "center",
+    justifyContent: "start",
+    borderWidth: 1,
+  }
 });
---
+++ README.md
@@ -0,0 +1,40 @@
+# REAMDE
+
+React Native: Assignment 1
+
+Explanation:
+- Nicname: enter the name
+- Age: enter the age
+- Like coffee: select "Yes" or "No"
+- Submit: Submit the form
+- Reset: Clear the details from the form
+
+# Screenshot
+
+Starting screen:
+
+![starting screen](./screenshots/app_1.jpg)
+
+After details are entered and submitted:
+
+![submitted](./screenshots/app_2.jpg)
+
+
+## Cloning
+
+After cloning, install the packages: 
+
+```
+npm install
+```
+
+
+
+## New packages
+
+```
+npx expo install react-native-safe-area-context
+
+npm install react-native-safe-area-context
+
+```
